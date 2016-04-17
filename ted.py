9c9bddb66f8c54b41c73b7c7a166ee46dda30ff0
LordBees <LordBees@users.noreply.github.com>
2016-04-17 21:13:50 +0100

    Added files via upload
    added clear all settings option and tweaked ui

--- ted.py
+++ ted.py
@@ -426,10 +426,11 @@ class Menu_settings_window:
         SKIPWARN_check = Checkbutton(OCL,text = 'skip openbox on link open',variable = self.Warn_Skiplink,onvalue = 1,offvalue =0)
         KEEPHISTORY_checks = Checkbutton(OCL,text = "DON'T keep history",variable = self.History_Track,onvalue = 1,offvalue =0)
         SESSIONHISTORY_checks = Checkbutton(OCL,text = "dont save session to disk",variable = self.Session_History,onvalue = 1,offvalue =0)
-
+        
         SAVESETTINGS_button = Button(OBL,text = 'Save settings',command = self.Menu_settings_savesettings)##'may be able to put into eventloop a check for the changes
         WIPESETTINGS_button = Button(OBL,text = 'clear settings',command = self.Menu_settings_wipesettings)
-
+        WIPEALLDATA_button = Button(OCL,text = "DEFAULT SETTINGS",command = self.menu_settings_default)
+        
         OCL.pack()
         OBL.pack()
 
@@ -439,6 +440,7 @@ class Menu_settings_window:
 
         SAVESETTINGS_button.pack()
         WIPESETTINGS_button.pack()
+        WIPEALLDATA_button.pack()
         
         #root.config()
         optionsmenu.title('options')
@@ -479,7 +481,7 @@ class Menu_settings_window:
         print(returner)
         return returner ##returning print(returner) may also work dunno tho
     
-    def Menu_settings_wipesettings(self):
+    def Menu_settings_wipesettings(self):##Reset config file
     #def save_file(name,data,overwrite = False,array = False):#saving funct
         data = self.array2csv(([0]*len(settings)))
 
@@ -489,7 +491,20 @@ class Menu_settings_window:
             #f.write(x+'\n')
         f.close()
         self.Menu_settings_loadsettings()#reload settings to memory
-
+        
+    def menu_settings_default(self):##reset to default ALL files
+        files = ['SETTINGS.CFF','CUSTOM.CLF','history.dat']
+        if messagebox.askokcancel(title = 'confirm WIPE',message = 'are you sure\nthis will reset the historyfile\ncustomfile and settings to default!'):
+            f = open('SETTINGS.CFF','w')
+            f.write('0,0,0,,')
+            f.close()
+            f = open('CUSTOM.CLF','w')
+            f.write('https://www.youtube.com/watch?v=,10,10,0.1.2.3.4.5.6.7.8.9.A.B.C.D.E.F.G.H.I.J.K.L.M.N.O.P.Q.R.S.T.U.V.W.X.Y.Z.a.b.c.d.e.f.g.h.i.j.k.l.m.n.o.p.q.r.s.t.u.v.w.x.y.z..,1,,')
+            f.close()
+            f = open('history.dat','w')
+            f.write('The History File')
+            f.close()##use mega for packing a backup?
+        print('wipe complete')
     ###these really shouldnt be here..
     def array2csv(self,array):##from beelib
         temp = ''
@@ -516,6 +531,8 @@ class Menu_settings_window:
         return arrayreturn
     ###
 
+
+
     
 class Menu_preview_window:
     PREVIEW_linkname = StringVar()
@@ -532,6 +549,9 @@ class Menu_preview_window:
         #root.config()
         previewmenu.title('preview')
         previewmenu.mainloop()
+
+
+
         
 class Menu_customchoose_window:
     Custom_linkprefix = StringVar()
